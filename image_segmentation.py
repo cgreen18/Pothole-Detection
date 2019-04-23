db4ff36e2d29f73e85a7f7076cac749a95870f2b
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2019-04-22 18:09:45 -0700

    Updated

--- image_segmentation.py
+++ image_segmentation.py
@@ -6,36 +6,31 @@ Version:
 1.0 - April 15 2019 - Created structure
 1.1 - April 21 2019 - Added many functions. Compiles and nearly operates as intended
 1.2 - April 22 2019 - Further modified the segmentation and filtering
+1.3 - April 22 2019 - Added averaging and better file manipulation.
 '''
 
 import cv2
-
-def get_avg_val(hsv_image):
-    sum = 0
-    h , w , d = hsv_image.shape
-
-    for i in range(0,h):
-        for j in range(0,w):
-            sum += hsv_image[i,j,2]
-
-    avg_value = sum/(w*h)
-
-    return avg_value
+import os
+import numpy
+#import matplotlib.pyplot as plt
 
 def perform_average_on_image(hsv_image):
-    avg = get_avg_val(hsv_image)
-
     h , w , d = hsv_image.shape
+
+    mod_hsv_image = numpy.copy(hsv_image)
+
+    mean , std = cv2.meanStdDev(hsv_image)
 
     for i in range(0,h):
         for j in range(0,w):
-            hsv_image[i,j,2] = int(hsv_image[i,j,2] / avg)
+            for k in range (0,d):
+                mod_hsv_image[i,j,k] = int((hsv_image[i,j,k] - mean[k]) / std[k])
 
-    return hsv_image
+    return mod_hsv_image
 
 def extract_road(hsv_image):
 
-    averaged_hsv_image = hsv_image # perform_average_on_image(hsv_image)
+    averaged_hsv_image = perform_average_on_image(hsv_image)
 
     mask = cv2.inRange(averaged_hsv_image,(0,0,0),(255,60,150))
 
@@ -48,6 +43,9 @@ def extract_road(hsv_image):
     return (proc_image , filter )
 
 def segment_filter(filter):
+    #cv2.countNonZero(filter[:,i,1])
+
+
     threshold = .5  #50%
 
     h , w , d = filter.shape
@@ -88,6 +86,11 @@ def segment_filter(filter):
 
     return bounds
 
+def get_connected_pts():
+
+
+    return
+
 def segment_im_file(image_path):
     im = cv2.imread(image_path)
 
@@ -100,9 +103,39 @@ def segment_im_file(image_path):
 
     bounds = segment_filter(im_filter)
 
-
     return (orig_image , proc_image , bounds)
 
+def get_pics_folder():
+    current_directory = os.getcwd()
+
+    #additional_path ="/bdd100k/images/100k/test"
+    #data_path = current_directory + additional_path
+
+    #addition_path_new = "/renamed_images"
+    #new_path = current_directory + addition_path_new
+
+    path = os.path.join(current_directory + "/renamed_images/")
+
+    return path
+
+def path_of_pic(number):
+    path = get_pics_folder()
+
+    pic_path = os.path.join( path + "image" + str(number) + ".jpg" )
+
+    return pic_path
+
+def get_rgb_from_path(path):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
+
+    return image
+
+def get_hsv_from_path(path):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image , cv2.COLOR_BGR2HSV)
+
+    return image
 
 def main():
 
