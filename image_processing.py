01ad6f96e76f464ec6a238203e4ab6a468e3fc71
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2019-04-22 18:09:29 -0700

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -5,6 +5,7 @@ Version:
 1.0 - April 8 2019 - General structure
 1.1 - April 15 2019 - Better involves paths and current directories
 1.2 -`April 22 2019 - Interacts with image_segmentation.py better`
+1.3 - April 22 2019 - Cleaner code
 '''
 
 import os
@@ -15,27 +16,6 @@ import time
 import renaming_files as rename
 import image_manipulation as imman
 import image_segmentation as imseg
-
-
-def get_pics_folder():
-    current_directory = os.getcwd()
-
-    #additional_path ="/bdd100k/images/100k/test"
-    #data_path = current_directory + additional_path
-
-    #addition_path_new = "/renamed_images"
-    #new_path = current_directory + addition_path_new
-
-    path = os.path.join(current_directory + "/renamed_images/")
-
-    return path
-
-def path_of_pic( number):
-    path = get_pics_folder()
-
-    pic_path = os.path.join( path + "image" + str(number) + ".jpg" )
-
-    return pic_path
 
 def plot_orig_and_proc(orig , proc, pause):
     plt.figure()
@@ -55,11 +35,19 @@ def plot_orig_and_proc(orig , proc, pause):
     return
 
 def main():
-    image_x_path = path_of_pic(20)
+    #image_x_path = path_of_pic(20)
 
-    (image_x , proc_image_x , bounds) = imseg.segment_im_file(image_x_path)
+    #(image_x , proc_image_x , bounds) = imseg.segment_im_file(image_x_path)
 
-    plot_orig_and_proc(image_x , proc_image_x , 5)
+    #plot_orig_and_proc(image_x , proc_image_x , 5)
+
+    im_2_path = imseg.path_of_pic(25)
+
+    im_2 = imseg.get_rgb_from_path(im_2_path)
+
+    (orig , proc , bounds) = imseg.segment_im_file(im_2_path)
+
+    plot_orig_and_proc(orig , proc , 5)
 
     return
 
